de069813054bf2878b2154a20f27fdaf3b7a5d88
anjsahu19 <anjsahu19@gmail.com>
2018-12-18 11:38:11 +0530

    Model Validation demo

--- ControllersDemo/Models/Student.cs
+++ ControllersDemo/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,17 @@ namespace ControllersDemo.Models
 {
     public class Student
     {
-        [DisplayName("Roll No")]
+        [DisplayName("Roll No")] [Required(AllowEmptyStrings =false,ErrorMessage ="Please add Roll No")]
         public int RollNo { get; set; }
-        [DisplayName("Name")]
+
+        [DisplayName("Name")] [Required(AllowEmptyStrings = false, ErrorMessage = "Please add Student Name")]
         public string Name { get; set; }
-        [DisplayName("Stream")]
+
+        [DisplayName("Stream")] [Required(AllowEmptyStrings =false, ErrorMessage = "Please add Stream")]
         public string DepartmentName { get; set; }
-        [DisplayName("Total Marks Obtained")]
+
+        [Required(AllowEmptyStrings =false, ErrorMessage = "Please add Total Marks Obtained by Student")]
+        [DisplayName("Total Marks Obtained")][Range(1,100,ErrorMessage ="Marks Should Be Between 1 to 100 Only")]
         public int TotalMarks { get; set; }
     }
 }
